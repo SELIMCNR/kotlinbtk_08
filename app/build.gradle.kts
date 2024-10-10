72047e39a7e093fd4917366e428c2d86dd72a788
Selim Çınar <btk02672@gmail.com>
2024-10-11 00:06:04 +0300

    update code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
+
     androidTestImplementation(libs.androidx.espresso.core)
 
 
